49b90c8afaa4728da1ec99c4825307a268ae4bb5
Abdelwahed <abdelwahedsouid@gmail.com>
2024-10-29 12:41:24 +0100

    chatbot design

--- src/app/pages/web-site/web-site-routing.module.ts
+++ src/app/pages/web-site/web-site-routing.module.ts
@@ -2,6 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {MainComponent} from "./fixed/main/main.component";
 import {HomeComponent} from "./home/home.component";
+import {ChatbotComponent} from "./chatbot/chatbot.component";
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {ServicesComponent} from "./services/services.component";
 
 
 const routes: Routes = [
@@ -9,7 +13,11 @@ const routes: Routes = [
     path: '',
     component: MainComponent,
     children: [
-      {path: 'home', component: HomeComponent},
+      {path: '', component: HomeComponent},
+      {path:'chatbot' , component: ChatbotComponent},
+      {path:'contact', component: ContactComponent},
+      {path:'about', component: AboutComponent},
+      {path:'services',component: ServicesComponent}
     ]
   }
 ];
